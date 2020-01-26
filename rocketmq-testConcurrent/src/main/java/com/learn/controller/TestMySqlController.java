63858ac3cb0e17e971b6337d6949fbfd6f260335
damon <540490410@qq.com>
2020-01-26 22:29:33 +0800

    新增redis配置

--- rocketmq-testConcurrent/src/main/java/com/learn/controller/TestMySqlController.java
+++ rocketmq-testConcurrent/src/main/java/com/learn/controller/TestMySqlController.java
@@ -5,16 +5,19 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.learn.entity.Demo;
 import com.learn.service.IDemoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/test")
 public class TestMySqlController {
+    @Autowired
+    private RedisTemplate redisTemplate;
     @Autowired
     private IDemoService demoService;
 
@@ -29,5 +32,15 @@ public class TestMySqlController {
         IPage<Demo> page = new Page<>(1,10);
         IPage<Demo> pageList = demoService.page(page);
         return pageList;
+    }
+
+    @GetMapping("redis")
+    public String redis(){
+//        redisTemplate.opsForValue().set("customer01","testValue");
+        String s = (String) redisTemplate.opsForValue().get("customer01");
+        System.err.println(s);
+        Set customer01 = redisTemplate.keys("customer*");
+        System.err.println(customer01);
+        return "cc";
     }
 }
